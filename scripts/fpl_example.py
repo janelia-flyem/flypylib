e5aeeb123655e922f6fc959bd10c7d19bc60e72c
Gary Huang <gbhuang@cs.umass.edu>
2017-04-19 10:45:11 -0400

    updated fpl_example to train on multiple subvolumes, compute p/r over multiple subvolumes

--- scripts/fpl_example.py
+++ scripts/fpl_example.py
@@ -5,33 +5,43 @@ import matplotlib.pyplot as plt
 network    = fplnetwork.FplNetwork(fplmodels.baseline_model, 18, 7, 4)
 
 base_dir   = '/groups/flyem/data/synapse_training/cx'
-train_data = (
-    '%s/pb26_03_mn135_std48_image.h5' % base_dir,
-    '%s/pb26_03_ru7_ri15_mn135_std48_im_' % base_dir )
+train_data = []
+for ii in (3,5,7):
+    train_data.append( (
+        '%s/pb26_%02d_mn135_std48_image.h5'     % (base_dir,ii),
+        '%s/pb26_%02d_ru7_ri15_mn135_std48_im_' % (base_dir,ii) ))
 generator  = fplobjdetect.gen_batches(
     train_data, network.rf_size, 32)
 
 network.train(generator, 1000, 10)
 
-train_json = '%s/pb26_03_synapses.json' % base_dir
-pred       = network.infer(train_data[0])
-out        = fplobjdetect.voxel2obj(pred, 27, 3, None, 5)
+train_json = []
+for ii in (3,5,7):
+    train_json.append('%s/pb26_%02d_synapses.json' % (base_dir,ii))
 
-gt         = fplsynapses.load_from_json(train_json)
+mm_train = []
+for ii in range(3):
+    pred       = network.infer(train_data[ii][0])
+    out        = fplobjdetect.voxel2obj(pred, 27, 5, None, 5)
+    gt         = fplsynapses.load_from_json(train_json[ii])
 
-mm_train   = fplobjdetect.obj_pr_curve(
-    out, gt, 27, np.arange(0.6,0.96,0.02) )
+    mm_train.append(fplobjdetect.obj_pr_curve(
+        out, gt, 27, np.arange(0.6,0.96,0.02) ))
+mm_train_agg = aggregate_pr(mm_train)
 
-test_data  = '%s/pb26_04_mn135_std48_image.h5' % base_dir
-test_json  = '%s/pb26_04_synapses.json' % base_dir
+mm_test = []
+for ii in (4,9):
+    test_data  = '%s/pb26_%02d_mn135_std48_image.h5' % (base_dir,ii)
+    test_json  = '%s/pb26_%02d_synapses.json'        % (base_dir,ii)
 
-pred       = network.infer(test_data)
-out        = fplobjdetect.voxel2obj(pred, 27, 3, None, 5)
+    pred       = network.infer(test_data)
+    out        = fplobjdetect.voxel2obj(pred, 27, 5, None, 5)
 
-gt         = fplsynapses.load_from_json(test_json)
+    gt         = fplsynapses.load_from_json(test_json)
 
-mm_test    = fplobjdetect.obj_pr_curve(
-    out, gt, 27, np.arange(0.6,0.96,0.02) )
+    mm_test.append(fplobjdetect.obj_pr_curve(
+        out, gt, 27, np.arange(0.6,0.96,0.02) ))
+mm_test_agg = aggregate_pr(mm_test)
 
-plt.plot(mm_train['rr'],mm_train['pp'],'b.')
-plt.plot(mm_test[ 'rr'],mm_test[ 'pp'],'r.')
+plt.plot(mm_train_agg['rr'],mm_train_agg['pp'],'b.')
+plt.plot(mm_test_agg[ 'rr'],mm_test_agg[ 'pp'],'r.')
