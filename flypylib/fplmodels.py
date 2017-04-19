f27610d2f5dd8f912a27fa7638c18877b2c3ccdf
Gary Huang <gbhuang@cs.umass.edu>
2017-04-19 10:42:47 -0400

    added vgg_like to fplmodels, this is close to the standard model that was being used in production with matconvnet/flymatlib

--- flypylib/fplmodels.py
+++ flypylib/fplmodels.py
@@ -41,3 +41,46 @@ def baseline_model(in_sz = None):
     model.add(Conv3D(1, (1,1,1), activation='sigmoid'))
 
     return model
+
+def vgg_like(in_sz = None):
+    """returns standard model based on VGG architecture"""
+
+    in_sz = fplutils.to3d(in_sz)
+    in_sz = in_sz + (1,)
+
+    model = Sequential()
+
+    model.add(Conv3D(48, (3,3,3), use_bias=False,
+                     input_shape=in_sz))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv3D(48, (1,1,1), use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2,2,2)))
+
+    model.add(Conv3D(48, (3,3,3), use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv3D(48, (1,1,1), use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2,2,2)))
+
+    model.add(Conv3D(48, (3,3,3), use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Conv3D(96, (1,1,1), use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Conv3D(96, (1,1,1), use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Conv3D(1, (1,1,1), activation='sigmoid'))
+
+    return model
