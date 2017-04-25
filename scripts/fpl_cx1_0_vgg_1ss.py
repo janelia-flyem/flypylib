6075ed0435a140736b6366afec08e5956453e97e
Gary Huang <gbhuang@cs.umass.edu>
2017-04-25 12:37:32 -0400

    added script for evaluating vgg-like model on new cx1 training/validation split

---
+++ scripts/fpl_cx1_0_vgg_1ss.py
@@ -0,0 +1,61 @@
+from flypylib import fplobjdetect, fplmodels, fplnetwork, fplsynapses
+import numpy as np
+import matplotlib.pyplot as plt
+
+# choose a net architecture
+#   possible models: baseline_model, vgg_like, resnet_like, unet_like
+model = fplmodels.vgg_like;
+
+network = fplnetwork.FplNetwork(model)
+is_mask = (model==fplmodels.unet_like)
+
+base_dir  = '/groups/flyem/data/synapse_training'
+train_idx = (28,)
+test_idx  = (54, 58, 60, 61, 65, 69, 70, 75, 95, 103)
+
+train_dir   = '%s/cx1_0_0' % base_dir
+train_data = []
+for ii in train_idx:
+    train_data.append( (
+        '%s/cx1_%03d_mn135_std48_image.h5'     % (train_dir,ii),
+        '%s/cx1_%03d_ru7_ri15_mn135_std48_im_' % (train_dir,ii) ))
+generator  = fplobjdetect.gen_batches(
+    train_data, network.rf_size, 32, is_mask)
+
+network.train(generator, 1000, 10)
+
+train_json = []
+for ii in train_idx:
+    train_json.append('%s/cx1_%03d_synapses.json' % (train_dir,ii))
+
+mm_train = []
+for ii in range(len(train_data)):
+    pred       = network.infer(train_data[ii][0])
+    out        = fplobjdetect.voxel2obj(pred, 27, 5, None, 5)
+    gt         = fplsynapses.load_from_json(train_json[ii])
+
+    mm_train.append(fplobjdetect.obj_pr_curve(
+        out, gt, 27, np.arange(0.6,0.96,0.02) ))
+mm_train_agg = fplobjdetect.aggregate_pr(mm_train)
+
+test_dir   = '%s/cx1_0_1' % base_dir
+mm_test = []
+for ii in test_idx:
+    test_data  = '%s/cx1_%03d_mn135_std48_image.h5' % (test_dir,ii)
+    test_json  = '%s/cx1_%03d_synapses.json'        % (test_dir,ii)
+
+    pred       = network.infer(test_data)
+    out        = fplobjdetect.voxel2obj(pred, 27, 5, None, 5)
+
+    gt         = fplsynapses.load_from_json(test_json)
+
+    mm_test.append(fplobjdetect.obj_pr_curve(
+        out, gt, 27, np.arange(0.6,0.96,0.02) ))
+mm_test_agg = fplobjdetect.aggregate_pr(mm_test)
+
+plt.plot(mm_train_agg.rr, mm_train_agg.pp, 'b-')
+plt.plot(mm_test_agg.rr,  mm_test_agg.pp,  'r-')
+plt.xlabel('recall')
+plt.ylabel('precision')
+plt.legend(['train', 'test'], loc='lower left')
+plt.show()
