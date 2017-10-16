8c92a43a62c2b3aa36400c58d64a24bc9ada28a6
Pat Gunn <pgunn01@gmail.com>
2017-10-16 15:19:35 -0400

    Give pydoc some love, refactor fpl_gib25_example further

--- flypylib/__init__.py
+++ flypylib/__init__.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+
+"""flypylib - A library for synapse/object detection in
+flyem-compatible biological images.
+
+This package exports the following subpackages:
+    fplerroranalysis
+    fplmodels
+    fplnetwork
+    fplobjdetect
+    fplsynapses
+They have their own documentation; "pydoc flypylib.fplmodels"
+will get you the fplmodels docs.
+
+Please see
+https://github.com/janelia-flyem/flypylib
+for the current sources. This software is most easily setup using
+the conda distribution of Python. 
+"""
+
 from .fplnetwork import FplNetwork
